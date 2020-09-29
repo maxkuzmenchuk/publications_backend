6d6fd196b44920985e693bd1f206a475fd27a497
maxkuzmenchuk <domovi4ok99@gmai.com>
2020-09-29 17:27:01 +0300

    init project

---
+++ src/main/java/com/kuzmenchuk/publications/PublicationsApplication.java
@@ -0,0 +1,13 @@
+package com.kuzmenchuk.publications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublicationsApplication.class, args);
+    }
+
+}
