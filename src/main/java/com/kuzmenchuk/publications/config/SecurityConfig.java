44a80b8ee27fa4dd7b21c0e9923199f0cb0413b9
maxkuzmenchuk <domovi4ok99@gmai.com>
2020-10-12 15:29:51 +0300

    publication features (add, update, delete)

--- src/main/java/com/kuzmenchuk/publications/config/SecurityConfig.java
+++ src/main/java/com/kuzmenchuk/publications/config/SecurityConfig.java
@@ -50,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("templates").permitAll()
                 .antMatchers("/", "/index", "/registration").permitAll()
                 .antMatchers("/admin/**").hasAnyAuthority("ROLE_ADMIN")
                 .antMatchers("/user/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
