af68a0fbbed734c8dd52b3b0e9cdc6992076511d
maxkuzmenchuk <domovi4ok99@gmail.com>
2020-11-30 17:07:05 +0300

    login with frontend

--- src/main/java/com/kuzmenchuk/publications/controller/UserController.java
+++ src/main/java/com/kuzmenchuk/publications/controller/UserController.java
@@ -3,23 +3,20 @@ package com.kuzmenchuk.publications.controller;
 import com.kuzmenchuk.publications.exception.UserAlreadyExistException;
 import com.kuzmenchuk.publications.repository.model.User;
 import com.kuzmenchuk.publications.service.UserService;
-import org.apache.tomcat.websocket.AuthenticationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.security.Principal;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -64,33 +61,9 @@ public class UserController {
         }
     }
 
-    @PostMapping("/login")
-    public void login(@RequestBody User user, BindingResult bindingResult, HttpServletRequest request) {
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(user.getUsername(),
-                user.getPassword());
-
-        // generate session if one doesn't exist
-        request.getSession();
-
-        token.setDetails(new WebAuthenticationDetails(request));
-        Authentication authenticatedUser = authenticationManager.authenticate(token);
-
-        SecurityContextHolder.getContext().setAuthentication(authenticatedUser);
-    }
-
-    @GetMapping("/login?error")
-    public String login(HttpServletRequest request, Model model) {
-        HttpSession session = request.getSession(false);
-        String errorMessage = null;
-        if (session != null) {
-            AuthenticationException ex = (AuthenticationException) session
-                    .getAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
-            if (ex != null) {
-                errorMessage = ex.getMessage();
-            }
-        }
-        model.addAttribute("errorMessage", errorMessage);
-        return "login";
+    @RequestMapping("/login")
+    public Principal user(Principal user) {
+        return user;
     }
 
     @GetMapping("profile/{id}")
