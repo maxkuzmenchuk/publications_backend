ce5e361c1ef70addb7d85b0b0dfa129e377a2eeb
mkuzmenchuk <maxkuzmenchuk@gmail.com>
2021-01-14 17:36:05 +0300

    tables sql

--- src/main/resources/PublicationsDatabase.sql
+++ src/main/resources/PublicationsDatabase.sql
@@ -62,3 +62,63 @@ alter table publications.user_role
 create unique index user_role_id_uindex
     on publications.user_role (id);
 
+-- Table: publications.orders
+
+-- DROP TABLE publications.orders;
+
+CREATE TABLE publications.orders
+(
+    id integer NOT NULL,
+    amount integer,
+    date timestamp without time zone,
+    price double precision,
+    status character varying(255) COLLATE pg_catalog."default",
+    user_id integer,
+    username character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT orders_pkey PRIMARY KEY (id)
+)
+
+    TABLESPACE pg_default;
+
+ALTER TABLE publications.orders
+    OWNER to postgres;
+
+-- Table: publications.order_details
+
+-- DROP TABLE publications.order_details;
+
+CREATE TABLE publications.order_details
+(
+    order_id integer NOT NULL,
+    publications_id integer,
+    CONSTRAINT fkjyu2qbqt8gnvno9oe9j2s2ldk FOREIGN KEY (order_id)
+        REFERENCES publications.orders (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+    TABLESPACE pg_default;
+
+ALTER TABLE publications.order_details
+    OWNER to postgres;
+
+-- Table: publications.publications
+
+-- DROP TABLE publications.publications;
+
+CREATE TABLE publications.publications
+(
+    id integer NOT NULL,
+    description character varying(255) COLLATE pg_catalog."default",
+    image_name character varying(255) COLLATE pg_catalog."default",
+    name character varying(255) COLLATE pg_catalog."default",
+    price integer,
+    CONSTRAINT publications_pkey PRIMARY KEY (id),
+    CONSTRAINT publications_price_check CHECK (price >= 0)
+)
+
+    TABLESPACE pg_default;
+
+ALTER TABLE publications.publications
+    OWNER to postgres;
+
